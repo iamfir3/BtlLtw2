bd2c45d9bfcddc230fec852c6ad3a3f0e90a50c9
iamfir3 <88335051+iamfir3@users.noreply.github.com>
2023-11-14 22:11:58 +0700

    init

---
+++ src/main/java/com/example/btlltw2/BtlLtw2Application.java
@@ -0,0 +1,13 @@
+package com.example.btlltw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtlLtw2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtlLtw2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/btlltw2/BtlLtw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btlltw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtlLtw2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
